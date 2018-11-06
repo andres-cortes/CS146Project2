d7e2d4d51fcd176a0987fa6f24aa45cad5dd26f5
andres-cortes <andres.cortes@sjsu.edu>
2018-11-06 11:16:17 -0800

    Added test classes

---
+++ MazeDrawerTester.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MazeDrawerTester
+{
+
+	@BeforeEach
+	void setUp() throws Exception
+	{
+	}
+
+	@AfterEach
+	void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
